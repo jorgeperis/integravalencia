9004309733a28d02dd6ac03ed8a703a1c57f38e4
Jorge Peris <jorgeperis@gmail.com>
2017-02-11 10:45:08 +0100

    Use slim html and create controller methods

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def start
+  end
+
+  def team
+  end
+
+  def setting_up
+  end
+
+  def services
+  end
+
+  def friend_webs
+  end
+
+  def blog
+  end
+  
+  def contact
+  end
+end
